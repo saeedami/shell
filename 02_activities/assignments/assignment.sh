15ee6a26dec795eb9c578a2f26df5a5b3b7e05fd
saeid amirzadeh <s.amirzadeh71@gmail.com>
2025-01-12 13:19:08 -0500

    coppying server files added

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,6 +38,8 @@ mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processe
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
+cp ./data/raw/*server*.log ./data/processed/server_logs
+
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
