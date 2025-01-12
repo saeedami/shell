3a8f550b5033f66004264b5708fdc41028f44074
saeid amirzadeh <s.amirzadeh71@gmail.com>
2025-01-12 13:21:02 -0500

    creating inventory text file added

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -54,7 +54,7 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
-
+ls ./data/processed/* > ./data/inventory.txt
 
 ###########################################
 
